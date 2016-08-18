9019c5bb0f6b127f159b540a5f2bcbd2c3d485dd
SeokJoon.Yun <seokjoon.yun@gmail.com>
2016-08-19 07:28:47 +0900

    Server class 분리 및 EventServer에 상속처리

---
+++ server.py
@@ -0,0 +1,43 @@
+from socket import socket, SO_REUSEADDR, SOL_SOCKET
+from asyncio import Task, coroutine
+from peer import Peer
+
+class Server(object):
+    def __init__(self, loop, port):
+        self.loop = loop
+        self._serv_sock = socket()
+        self._serv_sock.setblocking(0)
+        self._serv_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self._serv_sock.bind(('', port))
+        self._serv_sock.listen(5)
+        self._peers = []
+        Task(self._server())
+
+    def remove(self, peer):
+        self._peers.remove(peer)
+
+    def process(self, message):
+        pass
+
+    def broadcast(self, message):
+        for peer in self._peers:
+            peer.send(message)
+
+    @coroutine
+    def _server(self):
+        while True:
+            peer_sock, peer_name = yield from self.loop.sock_accept(self._serv_sock)
+            peer_sock.setblocking(0)
+
+            peer = self.get_peer(peer_sock, peer_name)
+            self.on_connected(peer)
+
+    def on_connected(self, peer):
+        self._peers.append(peer)
+
+        message = 'Peer %s connected!\n' % (peer.name,)
+        print(message)
+        self.broadcast(message)
+
+    def get_peer(self, peer_sock, peer_name):
+        return Peer(self, peer_sock, peer_name)
--- server_source.py
+++ server_source.py
@@ -1,6 +1,8 @@
 from socket import socket, SO_REUSEADDR, SOL_SOCKET
 from asyncio import Task, coroutine, get_event_loop
 from peer import Peer
+from server import Server
+
 
 HOST = 'localhost'
 PORT = 9090
@@ -11,48 +13,28 @@ BUFFER_SIZE = 1024
 
 class EventPeer(Peer):
     def on_received(self, bytes):
-        self._server.process(bytes)
+        self._server.process(self, bytes)
 
 
-class Server(object):
+class EventServer(Server):
     def __init__(self, loop, port, clients):
-        self.loop = loop
-        self._serv_sock = socket()
-        self._serv_sock.setblocking(0)
-        self._serv_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        self._serv_sock.bind(('', port))
-        self._serv_sock.listen(5)
-        self._peers = []
         self._clients = clients
-        Task(self._server())
+        super().__init__(loop, port)
 
     def remove(self, peer):
         print('EventPeer [{0}] quit.'.format(peer.name))
-        self._peers.remove(peer)
+        super().remove(peer)
 
-    def process(self, message):
-        print('%s: %s' % (self.name, bytes.decode(ENCODING)))
+    def process(self, peer, message):
+        print('%s: %s' % (peer.name, message.decode(ENCODING)))
         self._clients.process(message)
 
-    def broadcast(self, message):
-        for peer in self._peers:
-            peer.send(message)
-
-    @coroutine
-    def _server(self):
-        while True:
-            peer_sock, peer_name = yield from self.loop.sock_accept(self._serv_sock)
-            peer_sock.setblocking(0)
-            peer = EventPeer(self, peer_sock, peer_name)
-            self._peers.append(peer)
-
-            message = 'Peer %s connected!\n' % (peer.name,)
-            print(message)
-            self.broadcast(message)
+    def get_peer(self, peer_sock, peer_name):
+        return EventPeer(self, peer_sock, peer_name)
 
 
 def run_source_server(loop, clients):
-    server = Server(loop, 9090, clients)
+    server = EventServer(loop, 9090, clients)
 
     print(server)
     return server
